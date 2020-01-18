316ea8d860df018680e9a40119724c88c4e1ae17
Evgeniy <kamtorg@bk.ru>
2020-01-18 21:14:10 +0300

    Test Wrrap's routing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 #![deny(warnings)]
-use warp::Filter;
+use warp::*;
+use warp::http::Response;
 
 #[tokio::main]
 async fn main() {
-    let routes = warp::any().map(|| "Hello, Gate8!");
+    let main = warp::path("home").map(|| "Hello from Gate8!");
+    let create_tasks = warp::path("tasks")
+        .and(warp::post())
+        .map(|| {
+            Response::builder().body("{The task is saved!}")
+        });
+    let routes = main.or(create_tasks);
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
