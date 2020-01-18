65c7e528c6c4ad555c238cddc0d96f6cbdb4bf61
Evgeniy <kamtorg@bk.ru>
2020-01-18 16:07:00 +0300

    Test warp http server

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::net::TcpListener;
+#![deny(warnings)]
+use warp::Filter;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-
-    stream.read(&mut buffer).unwrap();
-
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", "Hello Gate8");
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+#[tokio::main]
+async fn main() {
+    let routes = warp::any().map(|| "Hello, Gate8!");
+    warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
