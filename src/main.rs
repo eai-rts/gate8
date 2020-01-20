9d5e9a9eb7cc61d1330e48faba02f348db250e8b
Evgeniy Pereverzev <kamtorg@bk.ru>
2020-01-20 16:04:49 +0300

    Test rust-kafka with docker-compose

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 #![deny(warnings)]
 use warp::*;
 use warp::http::Response;
+use std::fmt::Write;
+use std::time::Duration;
+use kafka::producer::{Producer, Record, RequiredAcks};
 
 #[tokio::main]
 async fn main() {
+    
+
     let main = warp::path("home").map(|| "Hello from Gate8!");
     let create_tasks = warp::path("tasks")
         .and(warp::post())
         .map(|| {
+            let mut producer = Producer::from_hosts(vec!("localhost:9092".to_owned()))
+                .with_ack_timeout(Duration::from_secs(1))
+                .with_required_acks(RequiredAcks::One)
+                .create()
+                .unwrap();
+            let mut buf = String::with_capacity(2);
+            for i in 0..10 {
+            let _ = write!(&mut buf, "{}", i);
+            producer.send(&Record::from_value("my-topic", buf.as_bytes())).unwrap();
+            buf.clear();
+        }
             Response::builder().body("{The task is saved!}")
         });
     let routes = main.or(create_tasks);
